b57167c71d9edfc2daf9ea7afe7308e05afcc264
Ayutac <skoch02@students.uni-mainz.de>
2022-04-28 17:22:29 +0200

    Basic set-up done!

---
+++ src/main/java/org/abos/fabricmc/xpmagic/XpMagic.java
@@ -0,0 +1,18 @@
+package org.abos.fabricmc.xpmagic;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class XpMagic implements ModInitializer {
+
+    public static final String MOD_ID = "xp-magic";
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    @Override
+    public void onInitialize() {
+        LOGGER.info("Initializing " + MOD_ID + "...");
+
+        LOGGER.info("Initializing of " + MOD_ID + " done!");
+    }
+}
